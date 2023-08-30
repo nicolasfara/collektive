cca2284dbd200900d8542ac5441cd4795336f17b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-08-30 15:56:34 +0200

    refactor: rewrite field implementation to avoid raw cast

--- dsl/src/commonMain/kotlin/it/unibo/collektive/stack/Stack.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/stack/Stack.kt
@@ -1,18 +1,13 @@
 package it.unibo.collektive.stack
 
 class Stack<X> {
-    private val currentStack: MutableList<X?> = mutableListOf()
-    fun currentPath(): Path = Path(currentStack.toList())
-    fun alignRaw(token: X?){
-        currentStack.add(token)
-    }
-    fun dealign(){
-        currentStack.removeLast()
-    }
+    private val currentStack = ArrayDeque<X?>()
 
-    override fun toString(): String {
-        return currentStack.toString()
-    }
+    fun currentPath(): Path = Path(currentStack.toList())
+    fun alignRaw(token: X?) = currentStack.addLast(token)
+    fun dealign() = currentStack.removeLast()
+
+    override fun toString(): String = currentStack.toString()
 }
 
 data class Path(val path: List<Any?>)
